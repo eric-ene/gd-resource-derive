7fd993803c3077f66a2733fea7feed97afb886cb
Eric Ene <eric03ene@gmail.com>
2025-08-07 16:29:33 -0400

    Refactoring TODO: Update README.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 extern crate proc_macro;
-use proc_macro::{ TokenStream };
+use proc_macro::TokenStream;
 use quote::quote;
 use syn;
 
@@ -13,13 +13,15 @@ pub fn derive_resource_fn(_item: TokenStream) -> TokenStream {
 fn impl_macro(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
     let data = &ast.data;
-    let syn::Data::Struct(data) = data else { unimplemented!() };
+    let syn::Data::Struct(data) = data else {
+        unimplemented!()
+    };
 
     let fields_names = data.fields.iter().map(|field| {
         let field_name = &field.ident;
 
         quote! {
-            #field_name,
+            #field_name: quote::format_ident!("_{}_final", #field_name),
         }
     });
 
@@ -28,26 +30,29 @@ fn impl_macro(ast: &syn::DeriveInput) -> TokenStream {
         let ty = &field.ty;
 
         quote! {
-            let res: Result<#ty, _> = self.get(stringify!(#f_name)).try_to();
-            let Some(#f_name) = res.ok() else {
-                return #name::default(); 
+            let _res = <#ty as FromGodot>::try_from_godot(via.get(stringify!(#f_name)));
+            let Some(quote::format_ident!("_{}_final", #f_name)) = _res.ok() else {
+                return Err(godot::prelude::ConvertError::new(format!("{} convert error", stringify!(#f_name))))
             };
         }
     });
 
-    let expanded =
-        quote! {
-            impl Into<#name> for godot::prelude::Resource {
-                fn into(self) -> #name {
-                    #(#fields_gd_impls)*
-                
+    let expanded = quote! {
+        impl GodotConvert for #name {
+            type Via = godot::prelude::Dictionary;
+        }
 
-                    #name {
-                        #(#fields_names)*
-                    }
-                }
-            } 
-        };
+        impl FromGodot for #name {
+            fn try_from_godot (via: Self::via) -> std::result::Result<Self, godot::prelude::ConvertError> {
+                #(#fields_gd_impls)*
+
+
+                Ok(Self {
+                    #(#fields_names)*
+                })
+            }
+        }
+    };
 
     expanded.into()
 }
